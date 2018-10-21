784e2cc90fa4d6a66ceff370bcef90b9de24399d
mdatsev <martin.datsev.p@gmail.com>
2018-10-21 11:08:01 +0300

    added files upload

--- server/app.py
+++ server/app.py
@@ -1,6 +1,15 @@
-from flask import Flask
+from flask import Flask, request
+from flask import render_template
+import os
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/", methods=["GET", "POST"])
+def upload():
+    if request.method == 'POST':
+        file1 = request.files['file1']
+        file2 = request.files['file2']
+        file1.save('./file1')
+        file2.save('./file2')
+        return render_template('index.html')
+    else:
+        return render_template('index.html')
