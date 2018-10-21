a24e6f19c60d66431480130c091a10ea4318afe9
Pe6oProgramista <p.milev3@gmail.com>
2018-10-21 14:25:11 +0300

    final

--- server/app.py
+++ server/app.py
@@ -6,12 +6,21 @@ import imageio
 
 app = Flask(__name__)
 
+@app.after_request 
+def add_header(r):
+	r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+	r.headers["Pragma"] = "no-cache"
+	r.headers["Expires"] = "0"
+	r.headers['Cache-Control'] = 'public, max-age=0'
+	return r
+
 @app.route("/", methods=["GET", "POST"])
 def upload():
 	if request.method == 'POST':
 		file1 = request.files['file1']
 		file2 = request.files['file2']
 		iterations = int(request.form['slider'])
+		print("---------------------------------------" + str(iterations))
 		file1.save('./file1')
 		file2.save('./file2')
 		
@@ -21,3 +30,4 @@ def upload():
 		return send_file('outfile.jpg', mimetype='image/jpg')
 	else:
 		return render_template('index.html')
+		
